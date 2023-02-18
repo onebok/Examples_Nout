276dc4f152dbf57e97b837dd36828914f1a84875
Alexandr Bochkaev <onebok@yandex.ru>
2023-02-18 18:21:03 +0700

    Комитим пример example004 деление двух чисел

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,7 +1,3 @@
-﻿// int numberA = 10;
-// int numberB = 5;
-
-double numberA = 12;
+﻿double numberA = 12;
 double numberB = 5;
-// int result = numberA + numberB;
 Console.WriteLine(numberA / numberB);
